40ea7fa3016315f09f6363aae32389d994e9bbbd
李增强 <2224357370@qq.com>
2022-05-26 17:05:45 +0800

    ✨ 新增动画使用

--- app/src/main/java/zengqiang/composestudy/module/AnimationActivity.kt
+++ app/src/main/java/zengqiang/composestudy/module/AnimationActivity.kt
@@ -1,15 +1,40 @@
 package zengqiang.composestudy.module
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.Text
+import androidx.compose.foundation.lazy.LazyColumn
+import zengqiang.composestudy.module.animator.AnimatedContentActivity
+import zengqiang.composestudy.module.animator.AnimatedVisibilityActivity
+import zengqiang.composestudy.widget.TextItem
 
 class AnimationActivity : ComponentActivity() {
+    private val data = arrayListOf("AnimatedVisibility", "AnimatedContent")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Text(text = "hello")
+            LazyColumn {
+                items(data.size, itemContent = {
+                    TextItem(msg = data[it]) {
+                        when (data[it]) {
+                            "AnimatedVisibility" -> startActivity(
+                                Intent(
+                                    this@AnimationActivity,
+                                    AnimatedVisibilityActivity::class.java
+                                )
+                            )
+                            "AnimatedContent" -> startActivity(
+                                Intent(
+                                    this@AnimationActivity,
+                                    AnimatedContentActivity::class.java
+                                )
+                            )
+                        }
+                    }
+                })
+
+            }
         }
     }
 }
